a10f15cd8b2e0cd034d3b8fe160e9f2aae0f1567
Zahra Karami <zahroo1680@gmail.com>
2024-05-30 22:58:36 +0330

    first commit

---
+++ functions/connect.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function connect_db(){
+    $server="mysql:host=localhost;dbname=term3_db;charset=utf8";
+    $user_db="root";
+    $pass_db="";
+    try {
+        $pdo= new PDO($server,$user_db,$pass_db);
+        //echo "اتصال به دیتابیس با موفقیت انجام شد"."<br>";
+    }
+    catch (Exception $e) {
+        echo $e->getMessage();
+    }
+    return $pdo;
+}
